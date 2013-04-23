55e2a1597da4487005a64367bc3d66be8dcb539d
Erik Heinisch <Imkerix@live.de>
2013-04-23 22:20:20 +0200

    Signed-off-by: Erik Heinisch <Imkerix@live.de>

--- pumpcontrol/src/Main.java
+++ pumpcontrol/src/Main.java
@@ -1,4 +1,18 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class Main {
+
+	/**
+	 * @param args
+	 */
+	private static final Logger log = Logger.getLogger(Main.class.getName());
+	private PumpControl MyPumpControl = new PumpControl();
+	
+	public static void main(String[] args) {
+		log.setLevel(Level.SEVERE);
+		System.setProperty("java.util.logging.config.file", "logging.properties");
+	}
 
 }
--- pumpcontrol/src/Pump.java
+++ pumpcontrol/src/Pump.java
@@ -1,4 +1,43 @@
+import java.util.logging.Logger;
+
 
 public class Pump {
+	private long runtimeInSec = 0;
+	private int datenleitung;
+	private long starttimenew;
+	private long endtimenew;
+	private static final Logger log = Logger.getLogger(Main.class.getName());
 
+	
+	Pump(int p_datenleitung){
+		datenleitung = p_datenleitung;
+		
+	}
+	public int changepowerstate(int p_powerstate){
+
+		if(p_powerstate==0){
+			//this.on();
+			starttimenew = System.currentTimeMillis() / 1000;
+			return 0;
+		}
+		if(p_powerstate==1){
+			//this.off(); //would disable pump
+			endtimenew = System.currentTimeMillis() / 1000;
+			runtimeInSec += endtimenew - starttimenew;
+			return 1;
+		}
+		else{
+			log.info("changepowerstate mit etwas anderem als 0 oder 1 aufgerufen");
+			return 3;
+		}
+	}
+	
+ 	public long getRuntime() {
+		return runtimeInSec;
+	}
+
+	public int getDatenleitung() {
+		return datenleitung;
+	}
+	
 }
