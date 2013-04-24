ea19a2a4655c2c6b7d9cf2340a2d62b09f0f93c3
Erik Heinisch <Imkerix@live.de>
2013-04-24 10:40:20 +0200

    + Basic stuff
    Signed-off-by: Erik Heinisch <Imkerix@live.de>

--- pumpcontrol/src/Pump.java
+++ pumpcontrol/src/Pump.java
@@ -41,4 +41,9 @@ public class Pump {
 		return datenleitung;
 	}
 	
+	public boolean notAus(){
+		//this.off();
+		return true;
+	}
+	
 }
--- pumpcontrol/src/PumpControl.java
+++ pumpcontrol/src/PumpControl.java
@@ -1,19 +1,29 @@
+import java.util.ArrayList;
+
 
 public class PumpControl {
 	
-	private Pump[] allpumps = new Pump[8];
+	private Pump[] allpumps;
+	private IOPortAccess theWorldOutside = new IOPortAccess();
 	private byte sperrbyte;
 	
 	PumpControl() {
-		
+		allpumps = new Pump[8];
+		this.mksperrbyte();
 	}
 	
 	public void Startpumping(){
-		
+		for(int i = 0; i<=allpumps.length; i++){		
+			theWorldOutside.writePort(i, sperrbyte >> i);
+		}
 	}
 	
 	public byte mksperrbyte(){
+		ArrayList<Pump> fifeLazyPumps = new ArrayList<Pump>();
 		
+		for(Pump p :allpumps){
+			
+		}
 		return 0;
 	}
 }
